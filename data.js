d9de3884655a67f8413f303ec9d3730b2264888d
Cássio <cassiocappellari@gmail.com>
2020-06-30 16:10:25 -0300

    backend is done

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id: 'starter',
+        logo: 'logo-starter',
+        name: 'Starter',
+        modules: '5 Modules',
+        price: 'Free'
+    },
+    {
+        id: 'launchbase',
+        logo: 'logo-launchbase',
+        name: 'LaunchBase',
+        modules: '16 Modules',
+        price: 'Paid'
+    },
+    {
+        id: 'gostack',
+        logo: 'logo-gostack',
+        name: 'GoStack',
+        modules: '15 Modules',
+        price: 'Paid'
+    }
+]
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const courses = require('./data')
 
 const server = express()
 
@@ -8,15 +9,29 @@ server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', function(req, res){
-    return res.render('about')
+    const data ={
+        rocketseatImg: '/assets/melhores-tecnologias.svg',
+        title: 'The best <strong id="green">technologies</strong> in <strong id="green">programming</strong>, straight to the point and in the <strong id="green">right way</strong>.',
+        about: 'In the middle of so much information and the amount of tools that come up every day, you need someone to take you in the right direction.',
+        buttonText: 'I want to board in this rocket',
+        socialMedia: [
+            {name: 'facebook'},
+            {name: 'instagram'},
+            {name: 'twitter'},
+            {name: 'youtube'}
+        ]
+    }
+
+    return res.render('about', {data})
 })
 
 server.get('/content', function(req, res) {
-    return res.render('content')
+    return res.render('content', {items: courses})
 })
 
 server.listen(5000, function(){
