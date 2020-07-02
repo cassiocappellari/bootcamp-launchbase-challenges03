b1a7cdb6b41b58d51b18483d33b081f4a4a0ffd5
Cássio <cassiocappellari@gmail.com>
2020-07-02 12:01:49 -0300

    challenge 3-3 done

--- data.js
+++ data.js
@@ -2,6 +2,7 @@ module.exports = [
     {
         id: 'starter',
         logo: 'logo-starter',
+        about: '100% free online courses to get you on the right foot in the most desired technologies on the market.',
         name: 'Starter',
         modules: '5 Modules',
         price: 'Free'
@@ -9,6 +10,7 @@ module.exports = [
     {
         id: 'launchbase',
         logo: 'logo-launchbase',
+        about: 'Master web development from scratch and accelerate towards your goals.',
         name: 'LaunchBase',
         modules: '16 Modules',
         price: 'Paid'
@@ -16,6 +18,7 @@ module.exports = [
     {
         id: 'gostack',
         logo: 'logo-gostack',
+        about: 'Master the back-end, front-end and mobile using the method that will guide you straight to the point towards your goals.',
         name: 'GoStack',
         modules: '15 Modules',
         price: 'Paid'
